ac38c42b5afef7ed2b46b1da288e949a1ee3eab9
Hashem <trainee.hashem.znati@sigmalabs.co.uk>
2026-06-26 13:57:31 +0100

    initial commit, with get_age() initial version complete along with Trainee class's __init__() initial version complete

---
+++ Level-1/task_one.py
@@ -0,0 +1,36 @@
+from datetime import date
+
+
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Returns their age in years as an int."""
+        today = date.today()
+        day, month, year = map(int, self.date_of_birth.split("-"))
+        age = today.year - year - ((today.month, today.day) < (month, day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        pass
+
+
+class Assessment:
+
+
+if __name__ == "__main__":
+    trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
+    print(trainee)
+    print(trainee.get_age())
+    trainee.add_assessment(Assessment(
+        "Python Basics", "multiple-choice", 90.1))
+    trainee.add_assessment(Assessment(
+        "Python Data Structures", "technical", 67.4))
+    trainee.add_assessment(Assessment("Python OOP", "multiple-choice", 34.3))
+    print(trainee.get_assessment("Python Basics"))
+    print(trainee.get_assessment("Python Data Structures"))
+    print(trainee.get_assessment("Python OOP"))
