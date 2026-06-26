2ffa7b18619184b4e1dee59c14569912ea9aa8d9
Hashem <trainee.hashem.znati@sigmalabs.co.uk>
2026-06-26 14:44:55 +0100

    level 1 complete, all of its tests passing, level 2 subclasses complete, working on edge cases for level 2 then testing will occur

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -1,6 +1,51 @@
 from datetime import date
 
 
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Returns their age in years as an int."""
+        today = date.today()
+        str_dob = self.date_of_birth.strftime("%d-%m-%Y")
+        day, month, year = map(int, str_dob.split("-"))
+        age = today.year - year - ((today.month, today.day) < (month, day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """
+        Appends the given assessment in the add_assessment 
+        arg to the assessments list.
+        """
+        self.assessments.append(assessment)
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        """
+        Loops through the assessment list and returns the assessment
+        matching the name in the arg, else None is returned.
+        """
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
+
+
+class Assessment:
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        self.type = type
+        self.score = score
+
+        if not isinstance(self.score, float):
+            raise ValueError("You must enter a float for assessment score!")
+        if self.type != "multiple-choice" and self.type != "presentation" and self.type != "technical":
+            raise ValueError(
+                "You must choose either multiple-choice, presentation or technical for assessment type.")
+
+
 if __name__ == "__main__":
     trainee = Trainee("Sigma", "trainee@sigmalabs.co.uk", date(1990, 1, 1))
     print(trainee)
--- Level-2/task_two.py
+++ Level-2/task_two.py
@@ -1,18 +1,72 @@
 from datetime import date
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 BELOW
-#
-#####
+class Trainee:
+    def __init__(self, name: str, email: str, date_of_birth: date):
+        self.name = name
+        self.email = email
+        self.date_of_birth = date_of_birth
+        self.assessments = []
+
+    def get_age(self) -> int:
+        """Returns their age in years as an int."""
+        today = date.today()
+        str_dob = self.date_of_birth.strftime("%d-%m-%Y")
+        day, month, year = map(int, str_dob.split("-"))
+        age = today.year - year - ((today.month, today.day) < (month, day))
+        return age
+
+    def add_assessment(self, assessment: Assessment) -> None:
+        """
+        Appends the given assessment in the add_assessment 
+        arg to the assessments list.
+        """
+        self.assessments.append(assessment)
+        if not isinstance(assessment, Assessment):
+            raise TypeError("You must enter an Assessment object!")
+
+    def get_assessment(self, name: str) -> Assessment | None:
+        """
+        Loops through the assessment list and returns the assessment
+        matching the name in the arg, else None is returned.
+        """
+        for assessment in self.assessments:
+            if assessment.name == name:
+                return assessment
 
 
-#####
-#
-# COPY YOUR CODE FROM LEVEL 1 ABOVE
-#
-#####
+class Assessment:
+    def __init__(self, name: str, type: str, score: float):
+        self.name = name
+        self.type = type
+        self.score = score
+
+        if not isinstance(self.score, float):
+            raise TypeError("You must enter a float for assessment score!")
+        if self.type != "multiple-choice" and self.type != "presentation" and self.type != "technical":
+            raise ValueError(
+                "You must choose either multiple-choice, presentation or technical for assessment type.")
+
+    def calculate_score(self, score: int):
+        return score * self.weighting
+
+
+class MultipleChoiceAssessment(Assessment):
+    def __init__(self, name: str, score: float):
+        super().__init__(name, score)
+        self.weighting = 0.7
+
+
+class PresentationAssessment(Assessment):
+    def __init__(self, name: str, score: float):
+        super().__init__(name, score)
+        self.weighting = 0.6
+
+
+class TechnicalAssessment(Assessment):
+    def __init__(self, name: str, score: float):
+        super().__init__(name, score)
+        self.weighting = 1
 
 
 if __name__ == "__main__":
